3c6cd6e46a4a028a43635aea3abe974f9d0aeaa6
RafM-M3 <rmierzejewski@m3global.com>
2022-01-23 14:00:24 +0000

    first

--- AutoconnectVPN.ps1
+++ AutoconnectVPN.ps1
@@ -1,6 +1,29 @@
+<#
+.SYNOPSIS
+A script to deploy scheduled task to autoconnect AOVPN Device Tunnel prior the user logon
+
+.DESCRIPTION
+This script will include checks for AOVPN bugs (User + Device) and do additional logging
+
+
+#>
+
+$GPO_Folder = "C:\Windows\AOVPN"
+$User_Tunnel_Name = "M3GR AOVPN" -replace ' ', '%20'
+$Device_Tunnel_Name = "M3GR AOVPN Device" -replace ' ', '%20'
+
+#In some cases when autoconnect checkbox is removed on the VPN user profile it will stop auto connecting
+function Reset_AutoTriggers(){
+    Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\RasMan\Config\ -Name AutoTriggerDisabledProfileList -Value "" -Type MultiString
+}
+
+#if AOVPN interface showing as *Unauthenticated* try to renew IP
+function Unauthenticated_Check(){
+    return [bool]$(Get-NetConnectionProfile | Where-Object {$_.InterfaceAlias -like "*Unauthenticated*"})
+}
+
 function ConnectVPN {
-    $vpnName = $VPNConnection.Name
-    $vpn = Get-VpnConnection -Name $vpnName;
+    $vpn = Get-VpnConnection -Name $Device_Tunnel_Name -AllUserConnection;
     if($vpn.ConnectionStatus -eq "Disconnected"){
-    rasdial $vpnName (Read-Host -Prompt "Enter VPN Username") (Read-Host -Prompt "Enter Password")
+    rasdial $Device_Tunnel_Name 
     }
